1d38c041858b5b408c00b28452a0158449658148
S M Al Hasib <alhasibsm@gmail.com>
2024-10-22 15:19:09 +0600

    chore: upgrade to next 15

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import {fetchInvoiceById, fetchCustomers} from '@/app/lib/data';
+import {fetchCustomers, fetchInvoiceById} from '@/app/lib/data';
 import {notFound} from 'next/navigation';
 import {Metadata} from 'next';
 
@@ -8,7 +8,8 @@ export const metadata: Metadata = {
     title: 'Edit Invoice',
 };
 
-export default async function Page({params}: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params;
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
